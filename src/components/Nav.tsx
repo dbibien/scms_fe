33b6eda5bec2157e494403cd8aa4c13bb02325bd
Bibi <dbibien@gmail.com>
2024-05-28 02:50:08 -0400

    feat: add responsive navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,13 @@
 import { Link } from "react-router-dom"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react"
 
 const Nav = () => {
   return (
@@ -7,23 +16,64 @@ const Nav = () => {
         <h1 className="text-white">SCMS</h1>
       </div>
 
-      <ul className="flex flex-row gap-4">
-        <li className="text-white">
-          <Link to="/home">Home</Link>
-        </li>
-        <li className="text-white">
-          <Link to="/users">Users</Link>
-        </li>
-        <li className="text-white">
-          <Link to="/concerns">Concerns</Link>
-        </li>
-        <li className="text-white">
-          <Link to="/reports">Reports</Link>
-        </li>
-      </ul>
+      <div className="flex flex-row gap-2">
+        <div className="hidden lg:block">
+          <AppNavLinks mobile={false} />
+        </div>
+
+        <div className="lg:hidden">
+          <MobileNav />
+        </div>
+      </div>
 
     </nav>
   )
 }
 
 export default Nav
+
+
+const NavListItem = ({ to, title }: { to: string, title: string }) => {
+  return (
+    <li className="text-white">
+      <Link to={to}>{title}</Link>
+    </li>
+  )
+}
+
+const AppNavLinks = ({ mobile = true }) => {
+  const mobileStyles = ""
+  const deskTopStyles = "flex flex-row gap-4"
+
+  return (
+    <ul className={mobile ? mobileStyles : deskTopStyles}>
+      <NavListItem to="/home" title="Home" />
+      <NavListItem to="/users" title="Users" />
+      <NavListItem to="/concerns" title="Concerns" />
+      <NavListItem to="/reports" title="Reports" />
+    </ul>
+  )
+}
+
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu color="#fff" />
+      </SheetTrigger>
+
+      <SheetContent side="top">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+
+        <AppNavLinks mobile={true} />
+      </SheetContent>
+    </Sheet>
+  )
+}
