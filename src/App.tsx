e7d44f5cdcce85539bb32e39218ec22857d70fd1
Bibi <dbibien@gmail.com>
2024-05-31 14:36:51 -0400

    add a login route and protect the other routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import './App.css'
-import { Outlet, Route, Routes } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav'
 import HomePage from './pages/Home'
 import UsersPage from './pages/Users'
 import ConcernsPage from './pages/Concerns'
 import ReportPage from './pages/Reports'
+import LoginPage from './pages/Login'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/login" element={<LoginPage />} />
+      
+      <Route path="/" element={<PrivateRoutes />}>
         <Route path="/home" element={<HomePage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/concerns" element={<ConcernsPage />} />
         <Route path="/reports" element={<ReportPage />} />
-        {/* Using path="*"" means "match anything", so this route
-          acts like a catch-all for URLs that we don't have explicit
-          routes for. 
-        */}
       </Route>
     </Routes>)
 }
@@ -35,4 +34,9 @@ const Layout = () => {
       </div>
     </>
   )
+}
+
+const PrivateRoutes = () => {
+  const auth = false
+  return auth ? <Layout /> : <Navigate to="/login" /> 
 }
