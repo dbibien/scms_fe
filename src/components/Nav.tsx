5ac491dafeaf274be7606a8c94094b2d6edda905
Bibi <dbibien@gmail.com>
2024-05-31 13:57:03 -0400

    add seperator line between the navigation links on the mobile view

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,6 +8,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { Menu } from "lucide-react"
+import { Separator } from "@radix-ui/react-separator"
 
 const Nav = () => {
   return (
@@ -38,9 +39,14 @@ const NavListItem = ({ to, title, mobile = true }: { to: string, title: string, 
   const deskTopStyles = "text-white"
 
   return (
-    <li className={mobile ? mobileStyles : deskTopStyles} >
-      <Link to={to}>{title}</Link>
-    </li>
+    <>
+      <li className={mobile ? mobileStyles : deskTopStyles} >
+        <Link to={to}>{title}</Link>
+      </li>
+
+
+      {mobile && <Separator orientation="horizontal" className="border border-slate-50"/> }
+    </>
   )
 }
 
@@ -50,14 +56,13 @@ const AppNavLinks = ({ mobile = true }) => {
 
   return (
     <ul className={mobile ? mobileStyles : deskTopStyles}>
-      <NavListItem to="/home" title="Home" mobile={mobile}/>
+      <NavListItem to="/home" title="Home" mobile={mobile} />
       <NavListItem to="/users" title="Users" mobile={mobile} />
-      <NavListItem to="/concerns" title="Concerns" mobile={mobile}/>
-      <NavListItem to="/reports" title="Reports" mobile={mobile}/>
+      <NavListItem to="/concerns" title="Concerns" mobile={mobile} />
+      <NavListItem to="/reports" title="Reports" mobile={mobile} />
     </ul>
   )
 }
-
 
 const MobileNav = () => {
   return (
@@ -66,7 +71,7 @@ const MobileNav = () => {
         <Menu color="#fff" />
       </SheetTrigger>
 
-      <SheetContent side="top">
+      <SheetContent side="right">
         <div>
           <AppNavLinks mobile={true} />
         </div>
