2895fdb14b1789b72509d10fb50346c354ae6b9a
Bibi <dbibien@gmail.com>
2024-07-06 18:48:36 -0400

    feat: only user's with the type of 'director' can edit and delete concerns

--- src/components/ConcernCard.tsx
+++ src/components/ConcernCard.tsx
@@ -2,12 +2,15 @@ import { concernType } from "@/common/types"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import ConcernCardEdit from "./ConcernCardEdit"
 import { Trash } from "lucide-react"
+import { useLoggedInUserStore } from "@/common/store"
 
 type concernCardType = {
   concern: concernType
 }
 
 const ConcernCard = ({ concern }: concernCardType) => {
+  const loggedInUserType = useLoggedInUserStore(state => state.user.type)
+
   return (
     <Card className="mb-4">
       <CardHeader>
@@ -18,11 +21,13 @@ const ConcernCard = ({ concern }: concernCardType) => {
       <CardContent>
         <p>{concern?.say.replace("<p>", "").replace("</p>", "")}</p>
       </CardContent>
+      {loggedInUserType === "director" ? (
+        <CardFooter className="flex gap-6">
+          <ConcernCardEdit concern={concern} />
+          <Trash className="text-slate-500 hover:text-red-400" />
+        </CardFooter>
+      ) : ""}
 
-      <CardFooter className="flex gap-6">
-        <ConcernCardEdit concern={concern} />
-        <Trash className="text-slate-500 hover:text-red-400" />
-      </CardFooter>
     </Card>
   )
 }
