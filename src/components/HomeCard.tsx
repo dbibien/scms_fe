aa383e7d282f241b7db0f497feee193c2c3662d1
Bibi <dbibien@gmail.com>
2024-07-27 04:37:09 -0400

    feat: A home with a missing phone number will inform the user on the home card

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -140,6 +140,17 @@ const HomeCard = ({ house, getHomeData }: homeCardType) => {
             </div>
           </>)
           }
+
+          {house?.phones.length === 0 && (<>
+            <Separator className="mt-6 border border-slate-200" />
+            <div className="mt-4">
+              <p className="text-sm underline text-slate-500">To-do:</p>
+              <div className="space-y-2 spce-x-2">
+                <p className="text-sm text-orange-400 p-2 inline-block rounded-md bg-orange-100">Primary phone number missing</p>
+                <p className="text-sm text-slate-500 p-2 inline-block rounded-md bg-sky-200 ml-2">Call pending</p>
+              </div>
+            </div>
+          </>)}
         </CardContent>
       </div>
 
@@ -203,7 +214,6 @@ const HomeCard = ({ house, getHomeData }: homeCardType) => {
                   }
                 </ScrollArea>
               )}
-
 
               <Sheet>
                 <SheetTrigger
