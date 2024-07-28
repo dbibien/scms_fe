5ce2847eb08af7944050b3a7ad73da89fcf8199c
Bibi <dbibien@gmail.com>
2024-07-27 20:15:18 -0400

    fix: properly informing the user when a primary phone number is missing and when there is a call pending

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -153,16 +153,16 @@ const HomeCard = ({ house, getHomeData }: homeCardType) => {
           </>)
           }
 
-          {house?.phones.length === 0 && (<>
+          {house?.phones.length === 0 || house?.pending_call_concerns_ids !== "" ? (<>
             <Separator className="mt-6 border border-slate-200" />
             <div className="mt-4">
               <p className="text-sm underline text-slate-500">To-do:</p>
               <div className="space-y-2 spce-x-2">
-                <p className="text-sm text-orange-400 p-2 inline-block rounded-md bg-orange-100">Primary phone number missing</p>
-                <p className="text-sm text-slate-500 p-2 inline-block rounded-md bg-sky-200 ml-2">Call pending</p>
+                {house?.phones.length === 0 && <p className="text-sm text-orange-400 p-2 inline-block rounded-md bg-orange-100">Primary phone number missing</p>}
+                {house?.pending_call_concerns_ids && <p className="text-sm text-slate-500 p-2 inline-block rounded-md bg-sky-200 ml-2">Call pending</p>}
               </div>
             </div>
-          </>)}
+          </>) : ""}
         </CardContent>
       </div>
 
