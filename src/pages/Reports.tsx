19883d836d1545894d8a355a8f27b01a7fa66e2e
Bibi <dbibien@gmail.com>
2024-08-01 15:22:28 -0400

    feat: add date label.

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,6 +1,26 @@
+import DatePicker from "@/components/DatePicker"
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+
 const ReportPage = () => {
-  return(
-  <div>Report page</div>
+  const [fromDate, setFromDate] = useState<Date | undefined>(undefined)
+  const [toDate, setToDate] = useState<Date | undefined>(undefined)
+
+
+  console.log("fromDate: ", fromDate)
+  console.log("toDate: ", toDate)
+
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Button>Create report</Button>
+      </div>
+
+      <div className="bg-red-50 mt-2 lg:flex lg:justify-center lg:gap-4">
+        <DatePicker label="From" date={fromDate} setDate={setFromDate} />
+        <DatePicker label="To" date={toDate} setDate={setToDate} />
+      </div>
+    </div>
   )
 }
 
