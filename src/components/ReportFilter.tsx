1b945aabe25289379672a44db5c127916d083ac1
Bibi <dbibien@gmail.com>
2024-08-01 16:30:57 -0400

    feat: add filter button on the upper left hand side to allow user to filter for report types

--- src/components/ReportFilter.tsx
+++ src/components/ReportFilter.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import DatePicker from "./DatePicker"
 import ReportTypePicker from "./ReportTypePicker"
+import { Filter } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 const ReportFilter = () => {
   const [fromDate, setFromDate] = useState<Date | undefined>(undefined)
@@ -12,11 +14,20 @@ const ReportFilter = () => {
   console.log("reportType: ", reportType)
 
   return (
-    <div className="bg-red-50 mt-2 lg:flex lg:justify-center lg:items-end lg:gap-4">
-      <DatePicker label="From" date={fromDate} setDate={setFromDate} />
-      <DatePicker label="To" date={toDate} setDate={setToDate} />
-      <ReportTypePicker label="Report type" setValue={setReportType} />
-    </div>
+    <Sheet>
+      <SheetTrigger className="flex flex-row gap-1 items-center">
+        <Filter />
+        Filter
+      </SheetTrigger>
+
+      <SheetContent side="bottom">
+        <div className="bg-red-50 mt-2 lg:flex lg:justify-center lg:items-end lg:gap-4">
+          <DatePicker label="From" date={fromDate} setDate={setFromDate} />
+          <DatePicker label="To" date={toDate} setDate={setToDate} />
+          <ReportTypePicker label="Report type" setValue={setReportType} />
+        </div>
+      </SheetContent>
+    </Sheet>
 
   )
 }
