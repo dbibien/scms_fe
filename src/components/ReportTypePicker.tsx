0398084d9b45e1d9577231f9c7452a755d42a3b3
Bibi <dbibien@gmail.com>
2024-08-01 16:06:07 -0400

    feat: ability to seelct a type of report to filter the results by

--- src/components/ReportTypePicker.tsx
+++ src/components/ReportTypePicker.tsx
@@ -1,6 +1,28 @@
-const ReportTypePicker = () => {
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+type CProps = {
+  setValue: React.Dispatch<React.SetStateAction<string>>,
+}
+
+const ReportTypePicker = ({ setValue }: CProps) => {
+
   return (
-    <p>Report type picker</p>
+    <Select onValueChange={(e) => setValue(e)}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Something</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
   )
 }
 
--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,14 +1,17 @@
 import DatePicker from "@/components/DatePicker"
+import ReportTypePicker from "@/components/ReportTypePicker"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
 
 const ReportPage = () => {
   const [fromDate, setFromDate] = useState<Date | undefined>(undefined)
   const [toDate, setToDate] = useState<Date | undefined>(undefined)
+  const [reportType, setReportType] = useState("")
 
 
   console.log("fromDate: ", fromDate)
   console.log("toDate: ", toDate)
+  console.log("reportType: ", reportType)
 
   return (
     <div>
@@ -19,6 +22,7 @@ const ReportPage = () => {
       <div className="bg-red-50 mt-2 lg:flex lg:justify-center lg:gap-4">
         <DatePicker label="From" date={fromDate} setDate={setFromDate} />
         <DatePicker label="To" date={toDate} setDate={setToDate} />
+        <ReportTypePicker setValue={setReportType} />
       </div>
     </div>
   )
