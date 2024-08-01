5063fda5d6a45de21d698edfd40804c0b50363e0
Bibi <dbibien@gmail.com>
2024-08-01 16:49:54 -0400

    feat: popover will auto close when quering backend for the filterd results

--- src/components/ReportFilter.tsx
+++ src/components/ReportFilter.tsx
@@ -4,18 +4,25 @@ import ReportTypePicker from "./ReportTypePicker"
 import { Filter } from "lucide-react"
 import { Popover } from "@radix-ui/react-popover"
 import { PopoverContent, PopoverTrigger } from "./ui/popover"
+import { Button } from "./ui/button"
 
 const ReportFilter = () => {
+  const [sheetOpen, setSheetOpen] = useState(false)
   const [fromDate, setFromDate] = useState<Date | undefined>(undefined)
   const [toDate, setToDate] = useState<Date | undefined>(undefined)
   const [reportType, setReportType] = useState("")
+
+  const handleFilterReports = () => {
+    console.log("Filtering reports")
+    setSheetOpen(false)
+  }
 
   console.log("fromDate: ", fromDate)
   console.log("toDate: ", toDate)
   console.log("reportType: ", reportType)
 
   return (
-    <Popover>
+    <Popover open={sheetOpen} onOpenChange={setSheetOpen}>
       <PopoverTrigger className="flex flex-row gap-1 items-center">
         <Filter />
         Filter
@@ -25,6 +32,12 @@ const ReportFilter = () => {
         <DatePicker label="From" date={fromDate} setDate={setFromDate} />
         <DatePicker label="To" date={toDate} setDate={setToDate} />
         <ReportTypePicker label="Report type" setValue={setReportType} styles="mt-2" />
+        <Button
+          className="mt-4 w-full"
+          onClick={handleFilterReports}
+        >
+          Filter
+        </Button>
       </PopoverContent>
     </Popover>
 
