c0358e7e57760a0d737fb9c5387cd5c61951b630
Bibi <dbibien@gmail.com>
2024-08-02 15:16:45 -0400

    feat: jsx of report card in progress

---
+++ src/components/ReportCard.tsx
@@ -0,0 +1,47 @@
+import { Card, CardContent, CardHeader } from "./ui/card"
+
+const ReportCardText = ({ title, content }: { title: string, content: string }) => {
+  return (
+    <div className="flex gap-1">
+      <p className="font-semibold">{title}: </p>
+      <p>{content}</p>
+    </div>
+  )
+}
+
+const ReportCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <p className="text-right text-sm text-slate-400">Aug 12 2025 12:09hrs</p>
+      </CardHeader>
+
+      <CardContent>
+        <div className="space-y-2">
+          <ReportCardText title="Officer" content="Bibien Dauphin" />
+          <ReportCardText title="Address" content="11117 Boca Wood Boca Raton, fl 44i879" />
+          <ReportCardText title="Type" content="Non-resident accident" />
+          <ReportCardText title="Weather" content="Rainny" />
+          <ReportCardText title="Resident" content="John Doeylucid" />
+          <ReportCardText title="Phone" content="+19546703788" />
+          <ReportCardText title="Member #" content="2343" />
+          <div className="flex justify-between">
+            <ReportCardText title="Injury" content="Yes" />
+            <ReportCardText title="EMS/PBSO" content="No" />
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <ReportCardText title="Narative" content="" />
+          <p>
+            skjdseoinnsneianoilagj gj gjaig g joipwd g g joispai  gjoapgajg a
+            jsdksoidigpwiespijg sgjopaijg ag  gjopagea opgjpajga g
+          </p>
+        </div>
+      </CardContent>
+
+    </Card>
+  )
+}
+
+export default ReportCard
--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,8 +1,9 @@
+import ReportCard from "@/components/ReportCard"
 import ReportFilter from "@/components/ReportFilter"
 import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const ReportPage = () => {
-
   return (
     <div>
       <div className="flex justify-between">
@@ -10,6 +11,22 @@ const ReportPage = () => {
         <Button>Create report</Button>
       </div>
 
+      <div>
+        {/*
+          <SplInput
+            type="text"
+            name="search"
+            searchValue={searchHomeValue}
+            setSearchValue={setSearchHomeValue}
+            placeHolder="Search homes..."
+            styles="pt-5 pb-5 text-lg"
+          />
+        */}
+
+        <ScrollArea className="h-[40rem] mt-2">
+          <ReportCard />
+        </ScrollArea>
+      </div>
     </div>
   )
 }
