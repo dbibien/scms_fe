72f203cae0b0d99013402a2af28609c02fb76d60
Bibi <dbibien@gmail.com>
2024-08-02 15:27:35 -0400

    fix: styles for better contrast

--- src/components/ReportCard.tsx
+++ src/components/ReportCard.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader } from "./ui/card"
 const ReportCardText = ({ title, content }: { title: string, content: string }) => {
   return (
     <div className="flex gap-1">
-      <p className="font-semibold">{title}: </p>
+      <p className="font-semibold text-gray-600">{title}: </p>
       <p>{content}</p>
     </div>
   )
@@ -22,9 +22,11 @@ const ReportCard = () => {
           <ReportCardText title="Address" content="11117 Boca Wood Boca Raton, fl 44i879" />
           <ReportCardText title="Type" content="Non-resident accident" />
           <ReportCardText title="Weather" content="Rainny" />
-          <ReportCardText title="Resident" content="John Doeylucid" />
-          <ReportCardText title="Phone" content="+19546703788" />
-          <ReportCardText title="Member #" content="2343" />
+          <div className="lg:flex justify-between">
+            <ReportCardText title="Resident" content="John Doeylucid" />
+            <ReportCardText title="Phone" content="+19546703788" />
+            <ReportCardText title="Member #" content="2343" />
+          </div>
           <div className="flex justify-between">
             <ReportCardText title="Injury" content="Yes" />
             <ReportCardText title="EMS/PBSO" content="No" />
