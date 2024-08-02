319e327fdf914f02c8d3a1a06bfd88a1e3579e7a
Bibi <dbibien@gmail.com>
2024-08-02 16:04:52 -0400

    add: moved ReportCard component into ReportCardList

---
+++ src/components/ReportCardList.tsx
@@ -0,0 +1,21 @@
+import ReportCard from "./ReportCard"
+import { ScrollArea } from "./ui/scroll-area"
+
+const ReportCardList = () => {
+  return (
+    <ScrollArea className="h-[80vh] mt-2">
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+    </ScrollArea>
+  )
+}
+
+export default ReportCardList
--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,7 +1,6 @@
-import ReportCard from "@/components/ReportCard"
+import ReportCardList from "@/components/ReportCardList"
 import ReportFilter from "@/components/ReportFilter"
 import { Button } from "@/components/ui/button"
-import { ScrollArea } from "@/components/ui/scroll-area"
 
 const ReportPage = () => {
   return (
@@ -23,13 +22,7 @@ const ReportPage = () => {
           />
         */}
 
-        <ScrollArea className="h-[80vh] mt-2">
-          <ReportCard />
-          <ReportCard />
-          <ReportCard />
-          <ReportCard />
-          <ReportCard />
-        </ScrollArea>
+        <ReportCardList />
       </div>
     </div>
   )
