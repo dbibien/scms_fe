c54145aef2587c3dccb1ee6b6d880531aa0dbc7b
Bibi <dbibien@gmail.com>
2024-08-18 19:08:41 -0400

    fix: modified the props of the SInput component to be able to handle numbers as well

--- src/components/SInput.tsx
+++ src/components/SInput.tsx
@@ -5,21 +5,25 @@ type CPropsTypes = {
   name: string,
   placeHolder: string,
   helperText?: string,
+  min?: number,
   max?: number,
+  maxCharacters?: number,
   // searchValue: string,
   // setSearchValue: React.Dispatch<React.SetStateAction<string>>,
   styles?: string,
   fields: any,
 }
 
-const SInput = ({ type, name, placeHolder, helperText = "", max = undefined, styles, fields }: CPropsTypes) => {
+const SInput = ({ type, name, placeHolder, helperText = "", maxCharacters = undefined, max = undefined, min = undefined, styles, fields }: CPropsTypes) => {
   return (
     <div>
       <Input
         type={type}
         name={name}
         placeholder={placeHolder}
-        maxLength={max}
+        maxLength={maxCharacters}
+        max={max}
+        min={min}
         // value={searchValue}
         className={`focus-visible:ring-0 ${styles}`} {...fields} />
       <p className="text-sm text-muted-foreground mt-1">{helperText}</p>
