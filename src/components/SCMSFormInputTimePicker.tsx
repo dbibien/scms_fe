dd0114df22f99523c03068f6e604969c91ba2559
Bibi <dbibien@gmail.com>
2024-08-26 02:51:49 -0400

    feat: helper message for the incident time inputs

--- src/components/SCMSFormInputTimePicker.tsx
+++ src/components/SCMSFormInputTimePicker.tsx
@@ -10,51 +10,54 @@ type CProps = {
 
 const SCMSFormInputTimePicker = ({ control }: CProps) => {
   return (
-    <div className="flex flex-row gap-2 items-center">
-      <FormField
-        control={control}
-        name="incidentTimeHour"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel></FormLabel>
-            <FormControl>
-              <SInput
-                type="number"
-                min={0}
-                max={23}
-                name="incidentTimeHour"
-                placeHolder="H"
-                styles="lg:w-18"
-                fields={field}
-              />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      :
-      <FormField
-        control={control}
-        name="incidentTimeMinute"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel></FormLabel>
-            <FormControl>
-              <SInput
-                type="number"
-                min={0}
-                max={59}
-                name="incidentTimeMinute"
-                placeHolder="M"
-                styles="lg:w-18"
-                fields={field}
-              />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
+    <>
+      <div className="flex flex-row gap-2 items-center">
+        <FormField
+          control={control}
+          name="incidentTimeHour"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel></FormLabel>
+              <FormControl>
+                <SInput
+                  type="number"
+                  min={0}
+                  max={23}
+                  name="incidentTimeHour"
+                  placeHolder="H"
+                  styles="lg:w-18"
+                  fields={field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        :
+        <FormField
+          control={control}
+          name="incidentTimeMinute"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel></FormLabel>
+              <FormControl>
+                <SInput
+                  type="number"
+                  min={0}
+                  max={59}
+                  name="incidentTimeMinute"
+                  placeHolder="M"
+                  styles="lg:w-18"
+                  fields={field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <p className="text-sm text-orange-300">Note: Incident time MUST be in 24 hour format</p>
+    </>
   )
 }
 
