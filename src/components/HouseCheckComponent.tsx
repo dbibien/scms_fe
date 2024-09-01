f7e872374feb1cf74ca846c7370a0a92c6249f0d
Bibi <dbibien@gmail.com>
2024-09-01 18:41:58 -0400

    feat: filtered the list of houses that needds to be checked from the list of houses in the global store

--- src/components/HouseCheckComponent.tsx
+++ src/components/HouseCheckComponent.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import SplInput from "./SplInput"
+import { ArrowDownUp } from "lucide-react"
+import HouseCheckList from "./HouseCheckList"
 
 const HouseCheckComponent = () => {
   const [searchValue, setSearchValue] = useState("")
 
   return (
     <div>
+      <ArrowDownUp />
+
       <SplInput
         type="text"
         name="search"
@@ -14,6 +18,8 @@ const HouseCheckComponent = () => {
         placeHolder="Address, type, resident, member number, etc..."
         styles="pt-5 pb-5 text-lg"
       />
+
+      <HouseCheckList />
     </div>
   )
 }
