0705bf3a9579e4514d5bb23b606dc8f4b952616b
Bibi <dbibien@gmail.com>
2024-09-01 23:49:41 -0400

    fix: add  data fetching logic

--- src/components/HouseCheckComponent.tsx
+++ src/components/HouseCheckComponent.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react"
 import SplInput from "./SplInput"
 import { ArrowDownUp } from "lucide-react"
 import HouseCheckList from "./HouseCheckList"
+import { useCommunityStore } from "@/common/store"
+import { useEffect, useState } from "react"
+import { houseType } from "@/common/types"
 
 const HouseCheckComponent = () => {
+  const allHouses = useCommunityStore(state => state.houses)
+
+  const [housesToBeChecked, setHousesToBeChecked] = useState<houseType[]>()
   const [searchValue, setSearchValue] = useState("")
+
+  const filterForHousesToBeChecked = () => (allHouses.filter(house => house?.house_check))
+
+  useEffect(() => {
+    setHousesToBeChecked(filterForHousesToBeChecked())
+  }, [])
 
   return (
     <div>
@@ -19,7 +30,9 @@ const HouseCheckComponent = () => {
         styles="pt-5 pb-5 text-lg"
       />
 
-      <HouseCheckList />
+      <HouseCheckList
+        housesToBeChecked={housesToBeChecked}
+      />
     </div>
   )
 }
