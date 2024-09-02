7a37a90a5d6e35b104d10aa0c370ad55faf6f92a
Bibi <dbibien@gmail.com>
2024-09-02 02:30:19 -0400

    feat: ability to hide and show the remark input field

--- src/components/HouseCheckCard.tsx
+++ src/components/HouseCheckCard.tsx
@@ -4,11 +4,46 @@ import HomeAddress from "./HomeAddress"
 import { Card, CardContent, CardTitle } from "./ui/card"
 import Note from "./Note"
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
-import { Label } from "./ui/label"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import STextArea from "./STextArea"
+import { Button } from "./ui/button"
+import Spinner from "./Spinner"
+import { useState } from "react"
+import { NotebookPen } from "lucide-react"
+
+const FormSchema = z.object({
+  everything_ok: z.enum(["yes", "no"], { required_error: "Required" }),
+  remark: z.string().optional()
+})
 
 const HouseCheckCard = ({ house }: { house: houseType }) => {
+  const [loading, setLoading] = useState(false)
+  const [showRemarkField, setShowRemarkField] = useState(false)
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  const watchedOk = form.watch("everything_ok")
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log("data: ", data)
+  }
+
+  // console.log("watchedOk: ", watchedOk)
+
   return (
-    <Card className="p-2">
+    <Card className="p-2 mb-8">
       <CardTitle className="text-lg text-slate-600">
         <HomeAddress house={house} />
       </CardTitle>
@@ -23,17 +58,90 @@ const HouseCheckCard = ({ house }: { house: houseType }) => {
         }
 
         <div className="mt-4">
-          <p className="text-md font-semibold mb-2">Is everything oK?</p>
-          <RadioGroup defaultValue="option-one">
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-one" id="option-one" />
-              <Label htmlFor="option-one">Yes</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="option-two" id="option-two" />
-              <Label htmlFor="option-two">No</Label>
-            </div>
-          </RadioGroup>
+          <Form {...form} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="everything_ok"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-md font-semibold mb-2">Is everything ok?</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        // defaultValue="option-one"
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="yes" />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            Yes
+                          </FormLabel>
+                        </FormItem>
+
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="no" />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            No
+                          </FormLabel>
+                        </FormItem>
+
+
+                      </RadioGroup>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <div className="mb-4">
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={() => setShowRemarkField(!showRemarkField)}
+                  >
+                    <NotebookPen className="text-slate-400 font-thin hover:text-black" />
+                  </button>
+                </div>
+
+                {showRemarkField && (
+                  <FormField
+                    control={form.control}
+                    name="remark"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-md font-semibold">Remark:</FormLabel>
+                        <FormControl>
+                          <STextArea
+                            name="remark"
+                            placeHolder="Remark regarding the home inspection"
+                            helperText=""
+                            styles="h-40"
+                            fields={field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+              </div>
+
+              <div className="">
+                <Button
+                  type="submit"
+                  disabled={loading || watchedOk === undefined}
+                  className="w-full"
+                >
+                  {loading ? <Spinner /> : "Submit"}
+                </Button>
+              </div>
+
+            </form>
+          </Form>
         </div>
       </CardContent>
     </Card>
