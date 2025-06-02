4385ccbfd5ed944ee664d3d8d41176eeb3e71337
Bibi <dbibien@gmail.com>
2025-06-02 03:59:08 -0400

    fix: HomeNotice tests

--- src/components/HomeNotice/HomeNotice.test.tsx
+++ src/components/HomeNotice/HomeNotice.test.tsx
@@ -239,7 +239,70 @@ describe('<HomeNotice />', () => {
     expect(screen.getByTestId("home-notice-callprog").textContent).toEqual("Call in progress...")
   })
 
-  it('should display the component AND the "House check" button when the house was last checked less than 7 days ago and the end has not yet passed', () => {
+  it('should display the component AND the "House check" button when it has been MORE than 7 days since the home was last checked and the end date for the house check has not yet passed', () => {
+    const testProps = {
+      phones: [{
+        id: "1",
+        phone_number: "555-555-5555",
+        primary: true,
+        type: "cell",
+      }] as phoneType[],
+      pending_call_concerns: "",
+      callInProgress: false,
+      house_check: true,
+
+      id: "",
+      address: "",
+      apt: "",
+      city: "",
+      state: "",
+      zip: "",
+      note: "",
+      house_check_note: "",
+      residents: [
+        {
+          id: "",
+          first_name: "",
+          last_name: "",
+          owner: false,
+        }
+      ],
+      houseCheckStartDate: new Date("April 01, 2025 00:00:00"),
+      houseCheckEndDate: new Date("April 30, 2025 23:59:59"),
+      houseLastChecked: new Date("April 03, 2025 23:59:59"),
+      now: new Date("April 13, 2025 23:59:59")
+    }
+
+    render(<HomeNotice
+      now={testProps.now}
+      phones={testProps.phones}
+      pending_call_concerns_ids={testProps.pending_call_concerns}
+      callInProgress={testProps.callInProgress}
+      house_check={testProps.house_check}
+
+      id={testProps.id}
+      address={testProps.address}
+      apt={testProps.apt}
+      city={testProps.city}
+      state={testProps.state}
+      zip={testProps.zip}
+      note={testProps.note}
+      house_check_note={testProps.house_check_note}
+      residents={testProps.residents}
+      houseCheckStartDate={testProps.houseCheckStartDate}
+      houseCheckEndDate={testProps.houseCheckEndDate}
+      houseLastChecked={testProps.houseLastChecked}
+    />)
+
+    expect(screen.getByTestId("home-notice-container")).not.toBeNull()
+    expect(screen.getByTestId("home-notice-separator")).not.toBeNull()
+    expect(screen.queryByTestId("home-notice-phones")).toBeNull()
+    expect(screen.queryByTestId("home-notice-pending")).toBeNull()
+    expect(screen.queryByTestId("home-notice-callprog")).toBeNull()
+    expect(screen.getByTestId("home-notice-houseCheck").textContent).toEqual("House check")
+  })
+
+  it('should NOT display the "House check" button when it has been LESS than 7 days since the last time the house was checked and the end date for the house check has not yet passed', () => {
     const testProps = {
       phones: [{
         id: "1",
@@ -270,9 +333,11 @@ describe('<HomeNotice />', () => {
       houseCheckStartDate: new Date("April 01, 2025 00:00:00"),
       houseCheckEndDate: new Date("April 30, 2025 23:59:59"),
       houseLastChecked: new Date("April 10, 2025 23:59:59"),
+      now: new Date("April 13, 2025 23:59:59")
     }
 
     render(<HomeNotice
+      now={testProps.now}
       phones={testProps.phones}
       pending_call_concerns_ids={testProps.pending_call_concerns}
       callInProgress={testProps.callInProgress}
@@ -325,10 +390,12 @@ describe('<HomeNotice />', () => {
       ],
       houseCheckStartDate: new Date("April 01, 2025 00:00:00"),
       houseCheckEndDate: new Date("April 30, 2025 23:59:59"),
-      houseLastChecked: new Date("April 10, 2025 23:59:59"),
+      houseLastChecked: new Date("April 02, 2025 23:59:59"),
+      now: new Date("April 13, 2025 23:59:59")
     }
 
     render(<HomeNotice
+      now={testProps.now}
       phones={testProps.phones}
       pending_call_concerns_ids={testProps.pending_call_concerns}
       callInProgress={testProps.callInProgress}
