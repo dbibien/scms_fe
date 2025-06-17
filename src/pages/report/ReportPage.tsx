979dbcb8d2c4ac37c9ebd104438a4bec56617f60
Bibi <dbibien@gmail.com>
2025-06-17 03:22:08 -0400

    fix: styled and restructured page

--- src/pages/report/ReportPage.tsx
+++ src/pages/report/ReportPage.tsx
@@ -1,25 +1,14 @@
-import HouseCheckComponent from "@/components/HouseCheckComponent"
+import PageHeader from "@/components/PageHeader/PageHeader"
 import ReportComponent from "@/components/ReportComponent"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Outlet } from "react-router-dom"
 
 const ReportPage = () => {
   return (
-    <Tabs defaultValue="reports" className="w-full">
-      <TabsList>
-        <TabsTrigger value="reports">Reports</TabsTrigger>
-        <TabsTrigger value="house_checks">House checks</TabsTrigger>
-      </TabsList>
-
-      <TabsContent value="reports">
-        <ReportComponent />
-        <Outlet />
-      </TabsContent>
-
-      <TabsContent value="house_checks">
-        <HouseCheckComponent />
-      </TabsContent>
-    </Tabs>
+    <>
+      <PageHeader title="Reports" />
+      <ReportComponent />
+      <Outlet />
+    </>
   )
 }
 
