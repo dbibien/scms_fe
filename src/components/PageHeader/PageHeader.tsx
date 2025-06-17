be5e1a33b56d7e2f208f1c8bed806e8796ae8673
Bibi <dbibien@gmail.com>
2025-06-17 02:38:16 -0400

    add: styles

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -5,7 +5,7 @@ const PageHeader = ({ title }: CProps) => {
   return (
     <h1
       data-testid="page-header"
-      className="text-2xl text-black"
+      className="text-2xl text-black font-bold mb-2"
     >
       {title}
     </h1>
