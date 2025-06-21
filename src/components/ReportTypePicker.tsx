5767ae49381a13296d39394d19265c2649423d19
Bibi <dbibien@gmail.com>
2025-06-21 06:04:49 -0400

    fix: report type picker now allows for a user to select more than one type of report

--- src/components/ReportTypePicker.tsx
+++ src/components/ReportTypePicker.tsx
@@ -7,13 +7,25 @@ import {
 } from "@/components/ui/select"
 import { REPORT_TYPES } from "@/common/utils"
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Checkbox } from "./ui/checkbox"
+import { Label } from "./ui/label"
+import { ReportFilterType } from "@/common/types"
+
 type CProps = {
   label: string,
   styles?: string,
+  reportFilterTypes: ReportFilterType[]
+  setReportFilterTypes: React.Dispatch<React.SetStateAction<ReportFilterType[]>>
   setValue: React.Dispatch<React.SetStateAction<string>>,
 }
 
-const ReportTypePicker = ({ label, styles = "", setValue }: CProps) => {
+const ReportTypePicker2 = ({ label, styles = "", setValue }: CProps) => {
   return (
     <div className={styles}>
       <p>{label}:</p>
@@ -27,6 +39,75 @@ const ReportTypePicker = ({ label, styles = "", setValue }: CProps) => {
           ))}
         </SelectContent>
       </Select>
+    </div>
+  )
+}
+
+// export ReportTypePicker2
+
+const ReportTypePicker = ({ label, styles = "", setReportValue, reportFilterTypes, setReportFilterTypes }: CProps) => {
+
+  const isChecked = (checked: string | boolean, selectedType: ReportFilterType) => {
+    const newReportFilterType = reportFilterTypes.map(val => {
+      if (selectedType.id === val.id) {
+        if (checked) {
+          console.log("in here", "val.label", val.label)
+          return {
+            id: val.id,
+            value: val.value,
+            label: val.label,
+            isSelected: true,
+          }
+        } else {
+          return {
+            id: val.id,
+            value: val.value,
+            label: val.label,
+            isSelected: false,
+          }
+        }
+      } else {
+        return {
+          id: val.id,
+          value: val.value,
+          label: val.label,
+          isSelected: val.isSelected,
+        }
+      }
+    })
+
+    console.log("checked: ", checked, "selectedType: ", selectedType)
+    console.log("newReportFilterType: ", newReportFilterType)
+
+    return newReportFilterType
+  }
+
+  return (
+    <div className={styles}>
+      <p>{label}:</p>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="report-types">
+          <AccordionTrigger>Report types</AccordionTrigger>
+          <AccordionContent>
+            {reportFilterTypes.map(type => (
+              <div
+                key={type.id}
+                className="flex flex-row items-center gap-2 mb-4"
+              >
+                <Checkbox
+                  key={type.id}
+                  // checked={type.id}
+                  checked={type.isSelected}
+                  onCheckedChange={(checked) => setReportFilterTypes(isChecked(checked, type))}
+                >
+                  {type.label}
+                </Checkbox>
+                <Label htmlFor={`${type.id}`}>{type.label}</Label>
+              </div>
+            ))}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
